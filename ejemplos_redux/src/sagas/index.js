564e70ae5a03a5b0a3683be1667264e89d9a806e
Andy5am <andysam8@gmail.com>
2020-05-05 12:23:32 -0600

    Sagas petOwner

--- ejemplos_redux/src/sagas/index.js
+++ ejemplos_redux/src/sagas/index.js
@@ -3,11 +3,16 @@ import { fork, all } from 'redux-saga/effects';
 import { watchLoginStarted } from './auth';
 import { watchSayHappyBirthday } from './happyBirthday';
 
+import {watchPetOwnerFetchingStarted,watchPetOwnerCreation,watchPetOwnerDestruction} from './petOwner';
+
 
 function* mainSaga() {
   yield all([
     fork(watchLoginStarted),
     fork(watchSayHappyBirthday),
+    fork(watchPetOwnerCreation),
+    fork(watchPetOwnerDestruction),
+    fork(watchPetOwnerFetchingStarted),
   ]);
 }
 
